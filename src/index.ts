2dfef0662a5818bf4cbf1fa6a92cc97c637f2a3f
JaredvGraham <jgram153@icloud.com>
2024-07-29 17:35:52 -0400

    docker

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,12 @@ const handleScreenshot = async (url: string): Promise<Buffer> => {
       process.env.NODE_ENV === "production"
         ? process.env.PUPPETEER_EXECUTABLE_PATH
         : puppeteer.executablePath(),
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
   });
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: "networkidle0" });
